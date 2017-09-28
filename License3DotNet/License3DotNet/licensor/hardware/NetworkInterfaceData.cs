421b687a003412f36b645e4f68fa924d2b5e771b
contrben <contrben@students.zhaw.ch>
2017-09-28 14:29:14 +0200

    All necessary classes created and PGP library added

--- License3DotNet/License3DotNet/licensor/hardware/NetworkInterfaceData.cs
+++ License3DotNet/License3DotNet/licensor/hardware/NetworkInterfaceData.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace License3DotNet
+namespace License3DotNet.licensor.hardware
 {
-    public class Class1
+    class NetworkInterfaceData
     {
     }
 }
