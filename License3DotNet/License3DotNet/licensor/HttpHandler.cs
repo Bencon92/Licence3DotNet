c88e8b6dd3d1bc322dfd42cd80d26f9057865b3f
Contreras Benjamin (conr) <conr@zhaw.ch>
2017-10-03 13:44:11 +0200

    Comments added to HttpHandler class

--- License3DotNet/License3DotNet/licensor/HttpHandler.cs
+++ License3DotNet/License3DotNet/licensor/HttpHandler.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Net;
 
+/**
+ * A simple wrapper class to make it possible to mock the network use when revocation
+ * is tested. In tests a mock class extending this is injected.
+ *
+ */
 namespace License3DotNet.licensor
 {
     class HttpHandler
@@ -14,6 +19,14 @@ namespace License3DotNet.licensor
             return int.Parse(Convert.ToString(wr.StatusCode));
         }
 
+        /**
+         * This should be mocked when testing revocation not to wait for a
+         * connection build up to a remote server.
+         * 
+         * @param url the url to which connection is to be opened
+         * @return the connection
+         * @throws IOException if the connection can not be made
+         */
         HttpWebRequest openConnection(Uri url)
         {
             HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(url);
