a2408c7859c9c6c53728274b0c3f2c314bb41efe
Contreras Benjamin (conr) <conr@zhaw.ch>
2017-10-03 09:17:32 +0200

    NetworkInterfaceData class ported

--- License3DotNet/License3DotNet/licensor/hardware/NetworkInterfaceData.cs
+++ License3DotNet/License3DotNet/licensor/hardware/NetworkInterfaceData.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 
 namespace License3DotNet.licensor.hardware
 {
     class NetworkInterfaceData
     {
+        public String name;
+        byte[] hwAddress;
+
+        private NetworkInterfaceData(NetworkInterface networkInterface)
+        {
+            name = networkInterface.Name;
+            try
+            {
+                hwAddress = networkInterface.GetPhysicalAddress().GetAddressBytes();
+            } catch(SocketException e) {
+                throw new SystemException(e.ToString(), e);
+            }
+        }
     }
 }
