742223e2bece6c72123f6bafe68a15a7ce1b95fa
Contreras Benjamin (conr) <conr@zhaw.ch>
2017-10-03 13:41:04 +0200

    HttpHandler class ported

--- License3DotNet/License3DotNet/licensor/HttpHandler.cs
+++ License3DotNet/License3DotNet/licensor/HttpHandler.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
 
 namespace License3DotNet.licensor
 {
     class HttpHandler
     {
+        int getResponseCode(HttpWebRequest httpUrlConnection)
+        {
+            HttpWebResponse wr = (HttpWebResponse)httpUrlConnection.GetResponse();
+            return int.Parse(Convert.ToString(wr.StatusCode));
+        }
+
+        HttpWebRequest openConnection(Uri url)
+        {
+            HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(url);
+            return wr;
+        }
     }
 }
